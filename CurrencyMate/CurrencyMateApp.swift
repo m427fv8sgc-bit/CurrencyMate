0671e784c191e9c2d06543ca2f4b952d8c9f3917
Uttam Bhattacharjee <uttam.bhattacharjee.sp@dlh.de>
2026-05-24 13:20:48 +0530

    Initial Commit

---
+++ CurrencyMate/CurrencyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyMateApp.swift
+//  CurrencyMate
+//
+//  Created by Uttam Bhattcharjee on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrencyMateTests/CurrencyMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyMateTests.swift
+//  CurrencyMateTests
+//
+//  Created by Uttam Bhattcharjee on 24/05/26.
+//
+
+import Testing
+@testable import CurrencyMate
+
+struct CurrencyMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
